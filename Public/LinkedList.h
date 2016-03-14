9fdd481937e953c2584a651734ae7bad8bb0f54f
foocoder <slow295185031@gmail.com>
2016-03-14 19:27:59 +0800

    add Public Linked Lish

---
+++ Public/LinkedList.h
@@ -0,0 +1,120 @@
+// ---- Program Info Start----
+//FileName:     LinkedList.h
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-03-14 15:03:53
+// ---- Program Info End  ----
+
+#ifndef _LINKEDLIST_H_
+#define _LINKEDLIST_H_
+
+#include <vector>
+#include <iostream>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x):val(x),next(NULL){
+    }
+};
+
+class ListNodeClass{
+    //The Head Node is -1;
+    ListNode * pListHead;
+    public:
+    //Constructor
+    ListNodeClass():pListHead(new ListNode(-1)){
+        #ifdef DEBUG
+        std::cout<<"Default"<<std::endl;
+        #endif
+    }
+    ListNodeClass(const std::vector<int> &vec):pListHead(new ListNode(-1)){
+        if(vec.empty()) return;
+        ListNode * p = pListHead;
+        for(auto i:vec){
+            ListNode * pTmp = new ListNode(i);
+            p->next = pTmp;
+            p = pTmp;
+        }
+        #ifdef DEBUG
+        std::cout<<"Parameter"<<std::endl;
+        #endif
+    }
+
+    //Copy Constructor
+    ListNodeClass(const ListNodeClass & rhs):pListHead(rhs.pListHead){
+        #ifdef DEBUG
+        std::cout<< "Copy"<<std::endl;
+        #endif
+    }
+
+    //Assign Function
+    ListNodeClass & operator=(const ListNodeClass & rhs){
+        //Check self Assignment
+        if(&rhs == this){
+            return *this;
+        }
+
+        //First Destruct All of Data in self object;
+        //Or Memory Leak will Occur!
+        while(pListHead != NULL){
+            ListNode * pTmp = pListHead;
+            pListHead = pListHead->next;
+            delete pTmp;
+        }
+        //Copy Data From rhs to this;
+        //Deep Copy
+        pListHead = new ListNode(-1);
+        ListNode * pThis = pListHead;
+        ListNode * pRHS = rhs.pListHead->next;
+        while(pRHS != NULL){
+            ListNode * pTmp = new ListNode(pRHS->val);
+            pThis->next = pTmp;
+            pThis = pTmp;
+            pRHS  = pRHS->next;
+        }
+        #ifdef DEBUG
+        std::cout<<"Assignment"<<std::endl;
+        #endif
+
+        return *this;
+    }
+
+    //Destructor
+    ~ListNodeClass(){
+        while(pListHead != NULL){
+            ListNode * pTmp = pListHead;
+            pListHead = pListHead->next;
+            delete pTmp;
+        }
+        //Avoid Wild Pointer;
+        pListHead = NULL;
+        #ifdef DEBUG
+        std::cout<<"Destructor"<<std::endl;
+        #endif
+    }
+
+    //Get List Head
+    ListNode * getListHead(){
+        return pListHead->next;
+    }
+
+    //Print List Val
+    void printList(){
+        ListNode * p = pListHead->next;
+        std::cout<<"\n---PrintLinkedListStart---"<<std::endl;
+        if(p != NULL) {
+            while(p != NULL){
+                std::cout<<"("<<p->val<<")->";
+                p = p->next;
+            }
+            std::cout<<"(NULL)";
+        }else
+            std::cout<<"List is NULL";
+        std::cout<<"\n--- PrintLinkedListEnd ---"<<std::endl;
+    }
+};
+#endif
---
+++ Public/PublicTest.cpp
@@ -0,0 +1,50 @@
+// ---- Program Info Start----
+//FileName:     PublicTest.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-03-14 16:33:09
+// ---- Program Info End  ----
+
+#include <iostream>
+#include <vector>
+#include "LinkedList.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //Test LinkedList.h
+    vector<int> vec0 = {1,2,3,4,5};
+    vector<int> vec1 = {1};
+    vector<int> vec2;
+
+    cout<<"--Obj0---"<<endl;
+    ListNodeClass listNodeObj0 = ListNodeClass(vec0);//Parameter Constructor
+    cout<<"--Obj0---"<<endl;
+
+    cout<<"--Obj1---"<<endl;
+    ListNodeClass listNodeObj1 ; // Default Constructor
+    listNodeObj1 = vec1; //Assign Function Invoked
+    cout<<"--Obj1---"<<endl;
+
+    cout<<"--Obj2---"<<endl;
+    ListNodeClass listNodeObj2 = vec1; //Parameter Constructor
+    cout<<"--Obj2---"<<endl;
+
+    cout<<"--Obj3---"<<endl;
+    ListNodeClass listNodeObj3; // Default Constructor
+    cout<<"--Obj3---"<<endl;
+
+    cout<<"obj0:";
+    listNodeObj0.printList();
+    cout<<"obj1:";
+    listNodeObj1.printList(); // listNodeObj1 Double Free
+    cout<<"obj2:";
+    listNodeObj2.printList();
+    cout<<"obj3:";
+    listNodeObj3.printList();
+    return 0;
+}
