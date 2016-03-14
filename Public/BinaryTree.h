d03773b0eacea4fbd96cb1b2452ccc2242a2844d
foocoder <slow295185031@gmail.com>
2016-03-14 21:07:13 +0800

    add Uncompleted Public BinaryTree

---
+++ Public/BinaryTree.h
@@ -0,0 +1,35 @@
+// ---- Program Info Start----
+//FileName:     BinaryTree.h
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-03-14 19:50:05
+// ---- Program Info End  ----
+
+#ifndef _BINARYTREE_H_
+#define _BINARYTREE_H_
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class BinaryTreeClass{
+    TreeNode * root;
+    public:
+    //Constructor
+    BinaryTreeClass(){}
+    //Copy Constructor
+    BinaryTreeClass(const BinaryTreeClass & rhs){}
+    //Assign Operator
+    BinaryTreeClass & operator=(const BinaryTreeClass & rhs){}
+    //Destructor
+    ~BinaryTreeClass(){}
+
+};
+
+#endif
